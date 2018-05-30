c46baf8c91dd4d4da2e73b7d607ed2a663f7f7a1
Harriet <clerryha@gmail.com>
2018-05-30 11:57:06 +0300

    adding files to feature branch

---
+++ signup.js
@@ -0,0 +1,11 @@
+﻿
+// Get the sform
+var sform = document.getElementById('f01');
+
+// When the user clicks anywhere outside of the form, close it
+window.onclick = function(event) {
+    If (event.target == sform){
+        sform.style.display = "none";
+    }
+}
+
