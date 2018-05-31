d8d8bfb6f112fe801aed6cd0540c5a6bdea8fe18
Harriet <clerryha@gmail.com>
2018-05-31 18:14:19 +0300

    updating endpoints

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 credentials = [{'email':'msteve@admin.com','Password': 'admin'},
                {'email':'msteve@gmail.com', 'Password': 1234}]
 req_uest = []
-dictionary = {'Request Type':'car repair', 'Clients name':
+dictionary = {'Request_Type':'car repair', 'Clients name':
               'steph', 'requestID': 1}
 
 req_uest.append(dictionary)
@@ -13,7 +13,7 @@ req_uest.append(dictionary)
 #Login_form
 @app.route('/api/v1/Login', methods=['GET', 'POST'])
 def login():
-    return 
+    return
 
 @app.route('/api/v1/Signup', methods=['GET', 'POST'])
 def Signup():
@@ -32,9 +32,11 @@ def add_requests():
 def requests():
     return make_response(jsonify({'requests': req_uest}), 200)
 
-@app.route('/myrequests/api/v1/<int:requestID>', methods=['PUT'])
-def rID():
-    
+@app.route('/api/v1/<str:Request Type>', methods=['PUT'])
+def r_edit(Request_Type):
+    req_st = [requst for requst in req_uest if requst['Request_Type'] == Request_Type]
+    req_st[0]['Request_Type'] = request.json['Request_Type']
+    return jsonify({'Request_Type': req_st[0]})
 
 #starting the server
 if __name__ == '__main__':
