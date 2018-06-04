4974d4a57a43992eb1c52c4aff58f63890685db3
Harriet <clerryha@gmail.com>
2018-06-04 12:36:46 +0300

    project files

--- api/tests/test.py
+++ api/tests/test.py
@@ -12,3 +12,7 @@ class TestClass(unittest.TestCase):
         my_request = self.tester.get('api/v1/requests', content_type = 'application/json')
         request_resp = json.loads(my_request.data.decode())
         self.assertEqual(response, request_resp["requests"])
+
+    #check that users can add requests
+    def test_create_requests(self):
+            
