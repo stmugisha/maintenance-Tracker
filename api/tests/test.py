173ff24f0071d5e43ba938100851f3be537173d1
Harriet <clerryha@gmail.com>
2018-06-04 18:58:01 +0300

    testing api endpoints

--- api/tests/test.py
+++ api/tests/test.py
@@ -1,7 +1,7 @@
 import unittest
-from app.app import app
+from app.views import app
 import json
-#from flask_login import current_user
+
 class TestClass(unittest.TestCase):
     def setUp(self):
         self.tester = app.test_client(self)
@@ -16,64 +16,19 @@ class TestClass(unittest.TestCase):
 
     #checking that users can add requests
     def test_add_req(self):
-        response = self.tester.get('/api/v1/requests')
-        self.assertTrue(response.status_code, 201)
+        dta = {'request_type':'phone repair', 'clients_name':
+              'steve', 'requestID': 2}
+        response = self.tester.post('/api/v1/requests', content_type = 'application/json', 
+                                     data = json.dumps(dta))
+
+        self.assertEqual(response.status_code, 200)
 
     #check that incorrect request is not posted.
     def test_incorrect_req(self):
-        response = self.tester.get ('/api/v1/requests')
-        self.assertTrue(response.status_code, 400)
+        response = self.tester.post('/api/v1/requests')
+        self.assertEqual(response.status_code, 400)
 
-    # Ensure that the login page loads correctly
-    def test_login_page_loads(self):
-        response = self.tester.get('/api/v1/Login')
-        self.assertIn(b'Please login', response.data)
-
-    # Ensure login behaves correctly with correct credentials
-    def test_correct_login(self):
-        with self.tester:
-            response = self.tester.post(
-                '/api/v1/Login',
-                data=dict(email="steve@admin", password="admin"),
-                follow_redirects=True
-            )
-            self.assertIn(b'You were logged in', response.data)
-            self.assertTrue(current_user.name == "admin")
-            self.assertTrue(current_user.is_active())
-
-    # check that login works correctly with incorrect credentials
-    def test_incorrect_login(self):
-        response = self.tester.post(
-            '/api/v1/Login',
-            data=dict(username="wrong", password="wrong"),
-            follow_redirects=True
-        )
-        self.assertIn(b'Invalid username or password.', response.data)
-
-    # checking logout works correctly
-    def test_logout(self):
-        with self.tester:
-            self.tester.post(
-                '/api/v1/Login',
-                data=dict(username="admin", password="admin"),
-                follow_redirects=True
-            )
-            response = self.tester.get('/api/v1/logout', follow_redirects=True)
-            self.assertIn(b'You were logged out', response.data)
-            self.assertFalse(current_user.is_active())
-
-    # check that logout page requires user login
-    def test_logout_route_requires_login(self):
-        response = self.tester.get('/api/v1/logout', follow_redirects=True)
-        self.assertIn(b'Please log in to access this page', response.data)
-
-
-     # check that Flask was set up correctly
-    def test_index(self):
-        response = self.tester.get('/api/v1/Login', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
-
-    # check that index page requires user login
-    def test_main_route_requires_login(self):
-        response = self.tester.get('/api/v1/Login', follow_redirects=True)
-        self.assertIn(b'Please log in to access this page', response.data)
+    #check invalid request modification.
+    def test_edit_req(self):
+        response = self.tester.put('/api/v1/<string:request_type>', content_type = 'application/json',)
+        self.assertEqual(response.status_code, 400)
